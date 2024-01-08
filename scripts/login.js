ec9d60f3a7a38c0da1fef1e9911dc596ae739642
Charles Bedell <charlesbedell5@gmail.com>
2024-01-08 09:53:40 -0500

    css fixes, minor html changes, lost of js

--- scripts/login.js
+++ scripts/login.js
@@ -1,21 +1,22 @@
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-
-
-function userSignUp(e) {
-  const userFieldValue = document.getElementById('user-email').value;
-  const userPasswordValue = document.getElementById('user-password').value;
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById('')
+  const errDisplay = document.getElementById('error-display');
+  errDisplay.style.display = 'none';
+  
+  try {
+    const username = document.getElementById('user-email').value;
+    const password = document.getElementById('user-password').value;
+    const loginResponse = await axios.post('http://localhost:3000/user/login', {
+      username,
+      password
+    });
+    const token = loginResponse.data.token;
+    localStorage.setItem('accessToken', token);
+    window.location.href = 'company.html';
+  } catch (err) {
+    errDisplay.textContent = err.response.data.message;
+    errDisplay.style.display = 'block';
+  }
+  
+});
